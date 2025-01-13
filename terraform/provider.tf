c80e20e12173cbf8e83c25b5faf99f229224902c
Victor Vazhinskiy <vikvag1987@yandex.ru>
2025-01-13 20:29:12 +0000

    edit terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "yandex" {
   zone      = "ru-central1-b"
-  #token     = "y0_AgAAAAByT6HZAATuwQAAAAD52xy1AACuqt2IIL1MFbXSzjdo5Q48dte8A"
+  #token     = "XXXX"
   cloud_id  = "b1g2n9nn09e3d1hed4ao"
   folder_id = "b1gk5vqcp57s01rsvig1"
   service_account_key_file = "keyyc.json"
